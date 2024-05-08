622763e61f4a59e13d2d6dfce464c44261f02ff5
Alfredo R Pabon <alfredorpabon@Alfredos-MacBook-Pro.local>
2024-05-08 16:03:32 -0500

    Add HERO popovers

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,25 @@
 /*!
-* Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
+ */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+const popoverTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="popover"]'
+);
+const popoverList = [...popoverTriggerList].map(
+  (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+);
+
+const popover = new bootstrap.Popover(".popover-dismiss", {
+  trigger: "focus",
+});
+
+const myCarouselElement = document.querySelector("#myCarousel");
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false,
+});
